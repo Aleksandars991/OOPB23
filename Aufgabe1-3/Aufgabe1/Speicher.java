21b428f73f8463d7eeb43a14487b743e2109c01b
Fabio <e1526912@student.tuwien.ac.at>
2017-10-16 19:21:31 +0200

    Speicher Klasse created and partially implemented

---
+++ Aufgabe1-3/Aufgabe1/Speicher.java
@@ -0,0 +1,48 @@
+package com.company;
+
+
+
+public class Speicher {
+
+    private int kapazitaet;
+    private int max;
+
+    public Speicher(int kapazitaet, int max, int menge ){
+
+        this.kapazitaet=kapazitaet;
+        this.max=max;
+    }
+
+    private int zustand(){
+        return this.kapazitaet;
+    }
+
+    private void speichern( int menge ){
+        if(this.kapazitaet==this.max){
+
+            //protokoll ueberschuss
+            /*der speicher ist voll und kann nichts mehr einnehmen*/
+
+        }
+        else if((this.kapazitaet+menge)>this.max) {
+            this.kapazitaet=this.max;
+            //teil ist ueberflussig
+            /*der speicher ist voll und kann nichts mehr einnehmen*/
+
+        } else {
+            this.kapazitaet+=this.kapazitaet+menge;
+        }
+    }
+
+    private void zapfen(int quantity){
+        if(this.kapazitaet<quantity){
+            //wird teil hier genommen oder in generell nur fehlermeldung?
+
+        }
+        else{
+            this.kapazitaet-=quantity;
+
+        }
+    }
+    
+}
